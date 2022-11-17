1732e84ca585e6198fdb9b869aae73cc160b77eb
Stephanie Bertolo <stephanie.bertolo@gmail.com>
2022-11-17 17:40:35 -0300

    Ataque do jogador.

--- versaoStephII.py
+++ versaoStephII.py
@@ -31,23 +31,30 @@ tabuleiroComputCoord = criarTabuleiro(5, 10)
 tabuleiroJogadorFeed = criarTabuleiro(5, 10)
 tabuleiroComputFeed = criarTabuleiro(5, 10)
 
+
+embarcacoesJogad = 5  # Embarcações restantes
+embarcacoesComput = 5
+
 # Coordenadas do computador
+
 jogadasComputador = 0
 
 while jogadasComputador < 5:
 
-    linhasComput = random.randint(0, 4)
-    colunasComput = random.randint(0, 9)
+    linhaComput = random.randint(0, 4)
+    colunaComput = random.randint(0, 9)
 
-    for i in range(linhasComput):
-        for j in range(colunasComput):
-            if tabuleiroComputCoord[linhasComput][colunasComput] == 0:
-                tabuleiroComputCoord[linhasComput][colunasComput] = 3
+    for i in range(linhaComput):
+        for j in range(colunaComput):
+            if tabuleiroComputCoord[linhaComput][colunaComput] == 0:
+                tabuleiroComputCoord[linhaComput][colunaComput] = 3
                 jogadasComputador += 1
             else:
                 jogadasComputador = jogadasComputador
 
+
 # Coordenadas do jogador
+
 jogadasJogador = 0
 
 while jogadasJogador < 5:
@@ -67,12 +74,10 @@ while jogadasJogador < 5:
     else:
         tabuleiroJogadorCoord[linhaJogad][colunaJogad] = 3
         print("\nEste é o seu tabuleiro:")
-        imprimirTabuleiro(tabuleiroJogadorCoord)
+        #imprimirTabuleiro(tabuleiroJogadorCoord)
         jogadasJogador += 1
 
 
-embarcacoesJogad = 5
-embarcacoesComput = 5
 
 print('\nTabuleiro do Computador:')
 imprimirTabuleiro(tabuleiroComputFeed)
@@ -84,7 +89,9 @@ imprimirTabuleiro(tabuleiroJogadorFeed)
 print('=' * 30)
 print(f"Embarcações restantes: {embarcacoesJogad} \n")
 
+# Ataques
 
+whie
 
 # Ataque do computador
 
@@ -97,17 +104,46 @@ while ataquesComputador < 1:
 
     if tabuleiroJogadorCoord[linhaJogad][colunaJogad] != 0:
         print('Computador acertou! :(')
-        tabuleiroJogadorCoord[linhaJogad][colunaJogad] = 1
+        tabuleiroJogadorCoord[linhaJogad][colunaJogad] = 'X'
         embarcacoesJogad -= 1
     elif tabuleiroJogadorCoord[linhaJogad][colunaJogad] == 0:
         print('Computador errou! :)')
 
-    ataquesComputador = ataquesComputador + 1
+    ataquesComputador += 1
 
     print(linhaJogad, colunaJogad)
     print(embarcacoesJogad)
     imprimirTabuleiro(tabuleiroJogadorCoord)
 
 
+
+# Ataque do jogador
+
+ataquesJogador = 0
+
+while ataquesJogador < 1:
+
+    linhaComput = int(input('Escolha qual posição horizontal deseja atacar (0, 4): '))
+    colunaComput = int(input('Escolha qual posição vertical deseja atacar (0, 9): '))
+
+    if linhaComput < 0 or linhaComput > 4:
+        print("\nPosição inválida! Escolha uma posição entre 0 e 4 para linha e entre 0 e 9 para colunas:  \n")
+
+    elif colunaComput < 0 or colunaComput > 9:
+        print("\nPosição inválida! Escolha uma posição entre 0 e 4 para linha e entre 0 e 9 para colunas: \n")
+
+    elif tabuleiroComputCoord[linhaComput][colunaComput] != 0:
+        print('Você acertou! :)')
+        tabuleiroComputCoord[linhaComput][colunaComput] = 'X'
+        embarcacoesComput -= 1
+
+    elif tabuleiroComputCoord[linhaComput][colunaComput] == 0:
+        print('Você errou! :(')
+
+    ataquesJogador += 1
+
+    print(linhaComput, colunaComput)
+    print(embarcacoesComput)
+    imprimirTabuleiro(tabuleiroComputCoord)
 
 
